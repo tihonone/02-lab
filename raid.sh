ab77f33627a180ca8dee1e20fd061a95dbf2d526
tihonone <cloud@tikhonov.me>
2023-03-14 14:21:07 +0000

    adding readme changes

---
+++ raid.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo -i
+  yum install -y mdadm smartmontools hdparm gdisk
+  mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
+  mdadm --create --verbose /dev/md0 -l 6 -n 5 /dev/sd{b,c,d,e,f}
+  mkdir /etc/mdadm/
+  echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
+  mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
+  parted -s /dev/md0 mklabel gpt
+  parted /dev/md0 mkpart primary ext4 0% 20%
+  parted /dev/md0 mkpart primary ext4 20% 40%
+  parted /dev/md0 mkpart primary ext4 40% 60%
+  parted /dev/md0 mkpart primary ext4 60% 80%
+  parted /dev/md0 mkpart primary ext4 80% 100%
+  partprobe
+  for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done
+  sudo mkdir -p /raid/part{1,2,3,4,5}
+  for i in $(seq 1 5); do sudo mount /dev/md0p$i /raid/part$i; done
